23573a02a1c601c90d516cbe134dbcd05a3373b4
PanHongTao <1398703720@qq.com>
2020-03-30 00:37:45 +0800

    更新了射击模块（完全没用

--- test_tanks.py
+++ test_tanks.py
@@ -5,6 +5,8 @@
 @Author  : Fantuan
 @Software: PyCharm
 """
+import pygame
+
 import tanks
 import Tkinter as tk
 import os, threading
@@ -13,24 +15,57 @@ import os, threading
 def run(nr_of_players=1):
     tanks.game = tanks.Game()
     tanks.castle = tanks.Castle()
-    tanks.game.stage = 1
+    tanks.game.stage = 0
     tanks.game.nr_of_players = nr_of_players
     tanks.game.nextLevel()
+    player_move_gui()
 
 
+def run_gui_single():
+    th = threading.Thread(target=run, args=(1,))
+    th.setDaemon(True)  # 守护线程
+    th.start()
+
+
+def run_gui_double():
+    th = threading.Thread(target=run, args=(2,))
+    th.setDaemon(True)  # 守护线程
+    th.start()
+
+dirction = ("向上", "向右", "向下", "向左")
+tank_type = ("普通", "三倍移速", "超级子弹", "四倍血量")
+bullet_owner = ("我方", "敌方")
+bonus_type=("手雷", "无敌", "保护", "星星", "生命", "冻结")
+tile_type=("空", "砖", "铁", "水", "草", "冰")
 def get_info():
+
+    strbuf = ""
     for player in tanks.players:
-        # 玩家 位置 生命数 状态
-        print("玩家" + str((player.rect[0], player.rect[1])) + str(player.state))
+        strbuf += "玩家位置%s，移动方向：%s\n" % ((player.rect[0], player.rect[1]), dirction[player.direction],)
+
     for enemy in tanks.enemies:
-        # 敌人 位置 形态 路径
-        print("敌人" + str((enemy.rect[0], enemy.rect[1])) + str(enemy.type) + str(enemy.path))
+        strbuf += "敌人位置%s，类型%s，移动方向：%s，移动速度：%s,移动路径%s\n" % (
+        (enemy.rect[0], enemy.rect[1]), tank_type[enemy.type], dirction[enemy.direction], enemy.speed,
+        (enemy.path[0], enemy.path[-1]),)
     for bullet in tanks.bullets:
-        # 子弹 位置 归属 强度
-        print("子弹" + str((bullet.rect[0], bullet.rect[1])) + str(bullet.owner) + str(bullet.power))
-    print("BUFF" + str(tanks.bonuses))
-    print("LABELS" + str(tanks.labels))
-    print ("当前关卡" + str(tanks.game.level) + "地图\n" + str(tanks.game.level.mapr))
+        strbuf += "子弹位置%s，归属%s，移动方向：%s，移动速度：%s\n" % (
+        (bullet.rect[0], bullet.rect[1]), bullet_owner[bullet.owner], dirction[bullet.direction], bullet.speed,)
+    for bonus in tanks.bonuses:
+        strbuf += "BUFF类型%s\n"%(bonus_type[bonus.bonus],)
+    strbuf += "当前关卡%s\n"%(tanks.game.stage)
+    level = tanks.Level(tanks.game.stage)
+    print (level.obstacle_rects)
+    print (level.mapr)
+    for tile in level.mapr:
+        for player in tanks.players:
+            # pygame.Rect(position[0] - 8 , position[1] + 11, 8, 6).colliderect(castle.rect)
+            # print ("%s-%s")%(tile.topleft,(player.rect[0],player.rect[1]))
+            if tile.x ==player.rect[0] or tile.y==player.rect[1]:
+                print("[%s,%s]")%(tile.topleft,tile_type[tile.type])
+
+
+    print strbuf
+    return strbuf
 
 
 def get_output_doc():
@@ -39,11 +74,55 @@ def get_output_doc():
         print ("消灭" + str(player.trophies) + str(player.score))
 
 
-def player_move():
-    for player in tanks.players:
-        player.move(player.direction)
-        player.fire()
 
+def player_move():
+    while not tanks.game.game_over:
+        for player in tanks.players:
+            for enemy in tanks.enemies:
+                # dirction = ("向上", "向右", "向下", "向左")
+                for pos in enemy.path[8:]:
+                    x,y=pos[0],pos[1]
+                    is_fire=False
+
+                    if x==player.rect[0] and y<=player.rect[1]:
+                        #             在上面
+                        bullet_speed=5
+                        if player.superpowers > 0:
+                            bullet_speed = 8
+
+                        time_enemy=enemy.path.index(pos)/enemy.speed
+                        time_player=(player.rect[1]-y)/bullet_speed
+                        if(time_enemy==time_player):
+                            player.rotate(tanks.Tank.DIR_UP)
+                            is_fire=True
+
+                    elif x==player.rect[0] and y>=player.rect[1]:
+                        #             在下面
+                        player.rotate(tanks.Tank.DIR_DOWN)
+                        is_fire=True
+
+                    elif x>=player.rect[0] and y==player.rect[1]:
+                        #             在右边
+                        player.rotate(tanks.Tank.DIR_RIGHT)
+                        is_fire=True
+
+                    elif x <= player.rect[0] and y == player.rect[1]:
+                        #             在左边
+                        player.rotate(tanks.Tank.DIR_LEFT)
+                        is_fire=True
+                    if is_fire:
+                        print ("开火，目标是%s"%((x,y),))
+                        if player.state == player.STATE_ALIVE:
+                            player.fire()
+
+            # 获取上下左右能达到的坐标范围
+            available_positions=[]
+
+
+def player_move_gui():
+    th = threading.Thread(target=player_move)
+    th.setDaemon(True)  # 守护线程
+    th.start()
 
 # 作弊能力-直接下一关
 def cheatNextLevel():
@@ -67,29 +146,40 @@ def cheatSuperDefense():
 
 def gui():
     root = tk.Tk()
-    root.geometry('460x240')
+    root.geometry('640x320')
     root.title('自动化测试')
 
     lb1 = tk.Label(root, text='自动化测试')
     lb1.pack()
 
-    # btn1 = tk.Button(root, text='单人模式', command=threading.Thread(target=run_single).start())
-    btn1 = tk.Button(root, text='单人模式', command=run())
-    btn1.place(relx=0.1, rely=0.4, relwidth=0.3, relheight=0.1)
-    btn2 = tk.Button(root, text='双人模式', command=run())
-    btn2.place(relx=0.6, rely=0.4, relwidth=0.3, relheight=0.1)
 
-    btn2 = tk.Button(root, text='自动化开始', command=get_info)
-    btn2.place(relx=0.6, rely=0.4, relwidth=0.3, relheight=0.1)
-    btn2 = tk.Button(root, text='显示信息', command=get_info)
-    btn2.place(relx=0.6, rely=0.4, relwidth=0.3, relheight=0.1)
-    btn2 = tk.Button(root, text='结果分析', command=get_info)
-    btn2.place(relx=0.6, rely=0.4, relwidth=0.3, relheight=0.1)
+    btn1 = tk.Button(root, text='单人模式', command=run_gui_single)
+    btn1.place(relx=0.1, rely=0.1, relwidth=0.3, relheight=0.15)
+    btn2 = tk.Button(root, text='双人模式', command=run_gui_double)
+    btn2.place(relx=0.1, rely=0.4, relwidth=0.3, relheight=0.15)
 
-    txt = tk.Text(root)
-    txt.place(rely=0.6, relheight=0.4)
+    btn3 = tk.Button(root, text='自动化开始', command=player_move_gui)
+    btn3.place(relx=0.5, rely=0.1, relwidth=0.2, relheight=0.1)
+
+    def get_info_gui():
+        strbuf=get_info()
+        info.set(strbuf)
+    btn4 = tk.Button(root, text='显示信息', command=get_info_gui)
+    btn4.place(relx=0.5, rely=0.3, relwidth=0.2, relheight=0.1)
+
+    btn5 = tk.Button(root, text='结果分析', command=get_output_doc)
+    btn5.place(relx=0.5, rely=0.5, relwidth=0.2, relheight=0.1)
+
+    btn6 = tk.Button(root, text='下一关', command=cheatNextLevel)
+    btn6.place(relx=0.75, rely=0.1, relwidth=0.2, relheight=0.1)
+    btn7 = tk.Button(root, text='无限火力', command=cheatSuperPowers)
+    btn7.place(relx=0.75, rely=0.3, relwidth=0.2, relheight=0.1)
+    btn8 = tk.Button(root, text='超级防御', command=cheatSuperDefense)
+    btn8.place(relx=0.75, rely=0.5, relwidth=0.2, relheight=0.1)
+    info=tk.StringVar()
+    txt = tk.Entry(root,textvariable=info,state='readonly')
+    txt.place(rely=0.7,relwidth=1, relheight=0.3)
     root.mainloop()
-
 
 def terminal():
     FLAG = True
@@ -97,23 +187,24 @@ def terminal():
         message = raw_input("用户输入:")
         # run 单人游戏
         if message == "r1":
-            threading.Thread(target=run(1)).start()
+            threading.Thread(target=run,args=(1,)).start()
         elif message == "r2":
-            threading.Thread(target=run(2)).start()
+            threading.Thread(target=run,args=(2,)).start()
         # quit 退出
         elif message == "q":
             FLAG = False
         # get 获取信息
-        elif message == "get":
+        elif message == "g":
             get_info()
         # move 自动化测试开始
         elif message == "m":
-            threading.Thread(target=player_move()).start()
+            threading.Thread(target=player_move).start()
         # move 自动化测试开始
         elif message == "l":
             cheatNextLevel()
 
 
 if __name__ == "__main__":
-    terminal()
-    # gui()
+    # terminal()
+    gui()
+
